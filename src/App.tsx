bf83e5d6730fc61aaa9d331e8b7ea4c1f6f307ca
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-07-25 15:42:24 +0200

    moved event handlers to App file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { ChangeEvent, useState, SyntheticEvent} from 'react';
 import './App.css'
 import './components/Card/Card.css'
 import CardList from './components/CardList/CardList'
 import Search from './components/Search/Search' 
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };    
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    if(typeof result == "string"){
+      setServerError(result);
+    }
+    else if(Array.isArray(result.data)){
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
+  };
   return (
     <>
-      <Search/>
+      <Search onClick={onClick} search={search} handleChange={handleChange}/>
       <CardList/> 
     </>
   )
