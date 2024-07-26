a21682cb281460dc5501ffe1b19da1f83d0aa519
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-07-26 11:12:23 +0200

    rewrote request link, error message return

--- src/api.tsx
+++ src/api.tsx
@@ -4,20 +4,19 @@ import { CompanySearch } from "./company";
 interface SearchResponse{
     data: CompanySearch[];
 }
-export const searchCompanies = async (query : string) => {
-    try{
-        const data = await axios.get<SearchResponse>(
-            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
-        );
-        return data;
-    }
-    catch{
-        if(axios.isAxiosError(error)){
+    export const searchCompanies = async (query: string) => {
+        try {
+          const data = await axios.get<SearchResponse>(
+            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=PofiOsVr1Q63MEDnzoOJqDJIgQd8pucI`
+          );
+          return data;
+        } catch (error) {
+          if (axios.isAxiosError(error)) {
             console.log("error message: ", error.message);
-        }
-        else{
+            return error.message;
+          } else {
             console.log("unexpected error: ", error);
-            return "An unexpected error has occured"
+            return "An expected error has occured.";
+          }
         }
-    }
-    }
+      };
