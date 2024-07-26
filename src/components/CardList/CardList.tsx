641b7497fc7754308ead5be05fe0fbadc86a2299
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-07-26 11:13:33 +0200

    conditional rendering added to the component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import Card from '../Card/Card'
-interface Props{}
-const CardList : React.FC<Props> = (props:Props):JSX.Element => {
-    return(
-        <div>
-        <Card companyName='Apple' ticker='AAPL' price={135} />
-        <Card companyName='Microsoft' ticker='MSFT' price={190}/>
-        <Card companyName='Tesla' ticker='TSLA' price={350}/>
-        </div>
-    )
+import { CompanySearch } from '../../company';
+import {v4 as uuidv4} from "uuid";
+interface Props{
+    searchResults: CompanySearch[];
 }
+const CardList: React.FC<Props> = ({
+    searchResults
+  }: Props): JSX.Element => {
+    return (
+      <div>
+        {searchResults.length > 0 ? (
+          searchResults.map((result) => {
+            return (
+              <Card
+                id={result.symbol}
+                key={uuidv4()}
+                searchResult={result}
+              />
+            );
+          })
+        ) : (
+          <p>
+            No results!
+          </p>
+        )}
+      </div>
+    );
+  };
 export default CardList
