6d4349e5f84f230f3b8f50aa38773599dbe898c9
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-07-26 11:12:50 +0200

    added conditional rendering and props to CardList component

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { searchCompanies } from './api';
 function App() {
   const [search, setSearch] = useState<string>("");
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
+  const [serverError, setServerError] = useState<string | null>(null);
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
@@ -27,7 +27,8 @@ function App() {
   return (
     <>
       <Search onClick={onClick} search={search} handleChange={handleChange}/>
-      <CardList/> 
+      {serverError && <h1>{serverError}</h1>}
+      <CardList searchResults={searchResult}/> 
     </>
   )
 }
