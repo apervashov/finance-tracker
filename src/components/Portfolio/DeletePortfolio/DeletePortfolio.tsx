7266add3aa6742ce9c93e46a4cb1c664c672231d
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-07-29 09:25:57 +0200

    stock delete button

---
+++ src/components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { SyntheticEvent } from "react";
+
+interface Props {
+  onPortfolioDelete: (e: SyntheticEvent) => void;
+  portfolioValue: string;
+}
+
+const CardPortfolio = ({ onPortfolioDelete, portfolioValue }: Props) => {
+  return (
+    <>
+      <form onSubmit={onPortfolioDelete}>
+        <input hidden={true} value={portfolioValue} />
+        <button>x</button>
+      </form>
+    </>
+  );
+};
+
+export default CardPortfolio;
