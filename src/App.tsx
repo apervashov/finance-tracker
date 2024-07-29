2f39fbb9478e969c51d2f35109917f39a12ba249
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-07-29 09:24:58 +0200

    added search displaying and add,delete functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,55 @@
-import { ChangeEvent, useState, SyntheticEvent} from 'react';
-import './App.css'
-import './components/Card/Card.css'
-import CardList from './components/CardList/CardList'
-import Search from './components/Search/Search' 
-import { CompanySearch } from './company';
-import { searchCompanies } from './api';
+import { ChangeEvent, useState, SyntheticEvent } from "react";
+import "./App.css";
+import "./components/Card/Card.css";
+import CardList from "./components/CardList/CardList";
+import Search from "./components/Search/Search";
+import { CompanySearch } from "./company";
+import { searchCompanies } from "./api";
+import PortfolioList from "./components/Portfolio/PortfolioList/PortfolioList";
 
 function App() {
   const [search, setSearch] = useState<string>("");
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
-  const onPortfolioCreate = (e: SyntheticEvent) =>{
+  const onPortfolioCreate = (e: any) => {
     e.preventDefault();
-    console.log(e);
-  }; 
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
+  };
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter((value)=>{
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(removed);
+  };
   const onClick = async (e: SyntheticEvent) => {
     const result = await searchCompanies(search);
-    if(typeof result == "string"){
+    if (typeof result == "string") {
       setServerError(result);
-    }
-    else if(Array.isArray(result.data)){
+    } else if (Array.isArray(result.data)) {
       setSearchResult(result.data);
     }
     console.log(searchResult);
   };
-  
+
   return (
     <>
-      <Search onClick={onClick} search={search} handleChange={handleChange}/>
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
       {serverError && <h1>{serverError}</h1>}
-      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/> 
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
+      <PortfolioList portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
