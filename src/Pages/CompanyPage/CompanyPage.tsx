e8d3219813ae7e8e30b929e6e5b238aeaade3266
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-07-31 14:38:27 +0200

    created page files

---
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -0,0 +1,10 @@
+interface Props{}
+const CompanyPage = (props:Props) => {
+  return (
+   <>
+   Company Page
+   </>
+  )
+};
+
+export default CompanyPage;
---
+++ src/Pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,58 @@
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import { CompanySearch } from "../../company";
+import { searchCompanies } from "../../api";
+import Navbar from "../../components/Navbar/Navbar";
+import Hero from "../../components/Hero/Hero";
+import Search from "../../components/Search/Search";
+import CardList from "../../components/CardList/CardList";
+import PortfolioList from "../../components/Portfolio/PortfolioList/PortfolioList";
+
+interface Props{}
+const SearchPage = (props:Props) => {
+    const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string | null>(null);
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onPortfolioCreate = (e: any) => {
+    e.preventDefault();
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
+  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter((value)=>{
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(removed);
+  };
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    if (typeof result == "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
+  };
+  return (
+    <>
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
+      {serverError && <h1>{serverError}</h1>}
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
+      <PortfolioList portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />
+    </>
+  )
+};
+
+export default SearchPage;
