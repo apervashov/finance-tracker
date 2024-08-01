0924071f103ea31da848b5298f259c09e24903c5
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-08-01 20:17:22 +0200

    updated company page to have conditional rendering

--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,10 +1,41 @@
-interface Props{}
-const CompanyPage = (props:Props) => {
+import { useParams } from "react-router";
+import { CompanyProfile } from "../../company";
+import { useEffect, useState } from "react";
+import { getCompanyProfile } from "../../api";
+import Sidebar from "../../components/Sidebar/Sidebar";
+
+import CompanyDashboard from "../../components/CompanyDashboard/CompanyDashboard";
+import Tile from "../../components/Tile/Tile";
+
+interface Props {}
+const CompanyPage = (props: Props) => {
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+    getProfileInit();
+  }, []);
   return (
-   <>
-   Company Page
-   </>
-  )
+    <>
+      {company ? (
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard ticker={ticker!}>
+            <Tile title="Company Name" subTitle={company.companyName} />
+            <Tile title="Industry" subTitle={company.industry} />
+            <Tile title="Stock Price" subTitle={company.price} />
+            <Tile title="Market Cap" subTitle={company.mktCap} />
+          </CompanyDashboard>
+        </div>
+      ) : (
+        <div>Company not found.</div>
+      )}
+    </>
+  );
 };
 
 export default CompanyPage;
