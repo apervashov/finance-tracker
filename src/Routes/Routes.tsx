82856b962ef568f207fc1cba5c34d72465b5c300
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-08-01 20:19:12 +0200

    new routes created

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -3,6 +3,9 @@ import HomePage from "../Pages/HomePage/HomePage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
 import SearchPage from "../Pages/SearchPage/SearchPage";
 import App from "../App";
+import CompanyProfile from "../components/CompanyProfile/CompanyProfile";
+import IncomeStatement from "../components/IncomeStatement/IncomeStatement";
+import DesignPage from "../Pages/DesignGuide/DesignGuide";
 
 export const router = createBrowserRouter([
     {
@@ -11,7 +14,13 @@ export const router = createBrowserRouter([
         children: [
             {path: "", element: <HomePage/>},
             {path: "search", element: <SearchPage/>},
-            {path: "company/:ticker", element: <CompanyPage/>},
+            {path: "company/:ticker", 
+                element: <CompanyPage/>,
+            children:[
+            {path: "company-profile", element: <CompanyProfile/>},
+            {path: "income-statement", element: <IncomeStatement/>},
+            ]},
+            {path: "design-guide", element: <DesignPage/>}
         ]
     }
 ])
