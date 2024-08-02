297d5fbbbcc0d5126f51180730a76db58e9ed305
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-08-02 23:05:38 +0200

    incomeStatement request created

--- src/api.tsx
+++ src/api.tsx
@@ -1,44 +1,53 @@
 import axios from "axios";
-import { CompanyProfile, CompanySearch } from "./company";
+import { CompanyIncomeStatement, CompanyProfile, CompanySearch } from "./company";
 
-interface SearchResponse{
-    data: CompanySearch[];
+interface SearchResponse {
+  data: CompanySearch[];
 }
-    export const searchCompanies = async (query: string) => {
-        try {
-          const data = await axios.get<SearchResponse>(
-            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=PofiOsVr1Q63MEDnzoOJqDJIgQd8pucI`
-          );
-          return data;
-        } catch (error) {
-          if (axios.isAxiosError(error)) {
-            console.log("error message: ", error.message);
-            return error.message;
-          } else {
-            console.log("unexpected error: ", error);
-             return "An expected error has occured.";
-          }
-        }
-      };
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=PofiOsVr1Q63MEDnzoOJqDJIgQd8pucI`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An expected error has occured.";
+    }
+  }
+};
 
-      export const getCompanyProfile = async(query: string) => {
-        try{
-          const data = await axios.get<CompanyProfile[]>(
-            `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=PofiOsVr1Q63MEDnzoOJqDJIgQd8pucI`
-          );
-          return data;
-        }catch(error:any){
-          console.log("error from an API: ", error.message);
-        }
-        };
-        export const getKeyMetrics = async(query: string) => {
-          try{
-            const data = await axios.get<CompanyProfile[]>(
-              `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=PofiOsVr1Q63MEDnzoOJqDJIgQd8pucI`
-            );
-            return data;
-          }catch(error:any){
-            console.log("error message: ", error.message);
-          }
-          };
-      
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=PofiOsVr1Q63MEDnzoOJqDJIgQd8pucI`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error from an API: ", error.message);
+  }
+};
+export const getKeyMetrics = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=PofiOsVr1Q63MEDnzoOJqDJIgQd8pucI`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+export const getIncomeStatement = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyIncomeStatement[]>(
+      `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=40&apikey=PofiOsVr1Q63MEDnzoOJqDJIgQd8pucI`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
