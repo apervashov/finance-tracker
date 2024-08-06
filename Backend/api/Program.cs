7c6632c8ffc508a0d371bf25cdf56bc84c89a898
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-08-06 16:36:43 +0200

    connected StockRepository and IStockRepository  to a builder

--- Backend/api/Program.cs
+++ Backend/api/Program.cs
@@ -1,4 +1,6 @@
 using api.Data;
+using api.Interfaces;
+using api.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDBContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddScoped<IStockRepository, StockRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
