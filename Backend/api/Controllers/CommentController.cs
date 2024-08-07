60529812465d3eacfc2541f91710163622051485
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-08-07 15:36:41 +0200

    Created CommentController, added GetAll and GetById requests

---
+++ Backend/api/Controllers/CommentController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepository;
+        public CommentController(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepository.GetAllAsync();
+            var commentDTO = comments.Select(s=>s.ToCommentDTO());
+            return Ok(commentDTO);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var comment = await _commentRepository.GetByIdAsync(id);
+            if(comment == null)
+            {
+                return NotFound();
+            }
+            return Ok(comment.ToCommentDTO());
+        }
+    }
+}
