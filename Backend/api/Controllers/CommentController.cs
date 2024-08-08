b55160d5b6c25ed82056ec0071700378ab6d61ce
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-08-08 10:32:45 +0200

    Comments CRUD functions finished

--- Backend/api/Controllers/CommentController.cs
+++ Backend/api/Controllers/CommentController.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.DTO.Comment;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace api.Controllers
 {
@@ -13,25 +16,63 @@ namespace api.Controllers
     public class CommentController : ControllerBase
     {
         private readonly ICommentRepository _commentRepository;
-        public CommentController(ICommentRepository commentRepository)
+        private readonly IStockRepository _stockRepository;
+        public CommentController(ICommentRepository commentRepository, IStockRepository stockRepository)
         {
             _commentRepository = commentRepository;
+            _stockRepository = stockRepository;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var comments = await _commentRepository.GetAllAsync();
-            var commentDTO = comments.Select(s=>s.ToCommentDTO());
+            var commentDTO = comments.Select(s => s.ToCommentDTO());
             return Ok(commentDTO);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
             var comment = await _commentRepository.GetByIdAsync(id);
-            if(comment == null)
+            if (comment == null)
             {
                 return NotFound();
+            }
+            return Ok(comment.ToCommentDTO());
+        }
+
+        [HttpPost("{stockId}")]
+        public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDTO commentDTO)
+        {
+            if (!await _stockRepository.StockExist(stockId))
+            {
+                return BadRequest("Stock doesn't exist.");
+            }
+            var commentModel = commentDTO.ToCommentFromCreate(stockId);
+            await _commentRepository.CreateAsync(commentModel);
+            return CreatedAtAction(nameof(GetById), new { id = commentModel.Id }, commentModel.ToCommentDTO());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var commentModel = await _commentRepository.DeleteAsync(id);
+            if (commentModel == null)
+            {
+                return NotFound("Comment doesn't exist.");
+            }
+            return Ok(commentModel);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDTO updateDTO)
+        {
+            var comment = await _commentRepository.UpdateAsync(id, updateDTO.ToCommentFromUpdate());
+            if (comment == null)
+            {
+                NotFound("Comment not found.");
             }
             return Ok(comment.ToCommentDTO());
         }
