8557f1bd5d6bd5e1e4b1202781502820a769a230
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-08-08 10:33:36 +0200

    Update IStockRepository.cs

--- Backend/api/Interfaces/IStockRepository.cs
+++ Backend/api/Interfaces/IStockRepository.cs
@@ -15,5 +15,7 @@ namespace api.Interfaces
         Task<Stock> CreateAsync(Stock stockModel);
         Task<Stock> UpdateAsync(int id, UpdateStockRequestDTO stockDTO);
         Task<Stock?> DeleteAsync(int id);
+
+        Task<bool> StockExist(int id);
     }
 }
