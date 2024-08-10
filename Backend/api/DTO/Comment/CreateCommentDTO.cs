619294177461e1ae256bea46f4bf8759d7e9232d
Andrii Pervashov <51098401+videoman496@users.noreply.github.com>
2024-08-10 11:56:49 +0200

    added data validation

--- Backend/api/DTO/Comment/CreateCommentDTO.cs
+++ Backend/api/DTO/Comment/CreateCommentDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,13 @@ namespace api.DTO.Comment
 {
     public class CreateCommentDTO
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must have at least 5 characters.")]
+        [MaxLength(280, ErrorMessage = "Title can't be over 280 characters.")]
         public string Title { get; set; } = string.Empty;
+        [Required]
+        [MinLength(20, ErrorMessage = "Content must have at least 20 characters.")]
+        [MaxLength(1000, ErrorMessage = "Content part can't be over 1000 characters.")]
         public string Content { get; set; } = string.Empty;
     }
 }
